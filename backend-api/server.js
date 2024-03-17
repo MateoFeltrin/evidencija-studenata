163f5b8970812aa96c3a68a5941bdbfc5c2d1200
SkinnyBuffalo <yahoomateo090@gmail.com>
2024-03-18 00:35:05 +0100

    Dodam endpoint za dohvat svih trenutacnih stanara

--- backend-api/server.js
+++ backend-api/server.js
@@ -1,16 +1,37 @@
-const express = require('express');
-const app = express ();
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
 app.use(express.json());
-var fs = require('fs'); //require file system object
+var fs = require("fs"); //require file system object
+
+const connection = mysql.createConnection({
+  host: "student.veleri.hr",
+  user: "mfeltrin",
+  password: "11",
+  database: "mfeltrin",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("Server Listening on PORT:", port);
+  console.log("Server Listening on PORT:", port);
 });
 
 app.get("/status", (request, response) => {
-   const status = {
-      "Status": "Running"
-   };
-   response.send(status);
+  const status = {
+    Status: "Running",
+  };
+  response.send(status);
+});
+
+app.get("/api/all-stanar", (req, res) => {
+  connection.query("SELECT * FROM `stanar` RIGHT JOIN `boravak` ON `boravak`.`oib` = `stanar`.`oib` WHERE `boravak`.`datum_iseljenja` IS NULL", (error, results) => {
+    if (error) throw error;
+
+    res.send(results);
+  });
 });
