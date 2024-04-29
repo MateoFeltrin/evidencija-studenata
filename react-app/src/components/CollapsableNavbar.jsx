175f91cc482131896d69fecf1246d0715c0dc06b
SkinnyBuffalo <yahoomateo090@gmail.com>
2024-04-29 18:01:51 +0200

    Slozena prijava na fronti i autorizacija na collapsable navbaru

--- react-app/src/components/CollapsableNavbar.jsx
+++ react-app/src/components/CollapsableNavbar.jsx
@@ -1,4 +1,3 @@
-// Za importanje ikona otic ovdje https://react-icons.github.io/react-icons/search/#q=
 import { PiStudent } from "react-icons/pi";
 import { FaRegUser } from "react-icons/fa";
 import { MdHandyman } from "react-icons/md";
@@ -7,12 +6,64 @@ import { FaBed } from "react-icons/fa";
 import { FaList } from "react-icons/fa";
 import { FaBookReader } from "react-icons/fa";
 import { IoIosLogIn } from "react-icons/io";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { TbDoor } from "react-icons/tb";
-
+import { IoIosLogOut } from "react-icons/io";
+import { jwtDecode } from "jwt-decode";
 
 const CollapsableNavbar = () => {
+  // Function to check if the user is authenticated (if a token exists in local storage)
+  const isAuthenticated = () => {
+    // Check if the token exists in local storage
+    const token = localStorage.getItem("token");
+    return token !== null;
+  };
+
+  // Function to get the user's role from the token
+  const getUserRole = () => {
+    // Retrieve the token from local storage
+    const token = localStorage.getItem("token");
+    if (token) {
+      // Decode the token to access its payload
+      const decodedToken = jwtDecode(token);
+      // Return the user's role (assuming the payload contains the 'uloga' property)
+      return decodedToken.uloga;
+    }
+    // Return null if there is no token
+    return null;
+  };
+  // Functions to check the user's role
+  const isAdmin = () => {
+    const role = getUserRole();
+    return role === "admin";
+  };
+
+  const isStanar = () => {
+    const role = getUserRole();
+    return role === "stanar";
+  };
+
+  const isDomar = () => {
+    const role = getUserRole();
+    return role === "domar";
+  };
+
+  const isRecepcionar = () => {
+    const role = getUserRole();
+    return role === "recepcionar";
+  };
+
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Clear the token from local storage
+    localStorage.removeItem("token");
+    // Redirect the user to the PrijavaPage (login page)
+    navigate("/prijava");
+  };
+  // Check if a token exists in local storage
+  const tokenExists = localStorage.getItem("token");
 
   return (
     <nav className="navbar bg-body-tertiary">
@@ -32,87 +83,113 @@ const CollapsableNavbar = () => {
           </div>
           <div className="offcanvas-body">
             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item dropdown-center">
-              <li className="nav-item dropdown-center">
-                <button className={`nav-link dropdown-toggle icon-link ${location.pathname === "/popisSvihStanara" || location.pathname === "/popisVremenskogPeroida" ? "active" : ""}`} role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <PiStudent />
-                  Popisi studenata
-                </button>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className=" icon-link dropdown-item" aria-current="page" to="/popisSvihStanara">
-                      <FaList />
-                      Popis svih trenutačnih stanara
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className=" icon-link dropdown-item" aria-current="page" to="/popisVremenskogPeroida">
-                      <FaList />
-                      Popis stanara u vremenskom periodu
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <Link className={`nav-link icon-link ${location.pathname === "/PopisKorisnika" ? "active" : ""}`} aria-current="page" to="/PopisKorisnika">
-              <FaRegUser />
-              Popis korisnika
-            </Link>
-            <li>
-            <Link className={`nav-link icon-link ${location.pathname === "/PopisBoravaka" ? "active" : ""}`} aria-current="page" to="/PopisBoravaka">
-              <FaBookReader />
-              Popis Boravaka
-            </Link>
-            </li>
-            <li className="nav-item dropdown-center">
-                <button className={`nav-link dropdown-toggle icon-link ${location.pathname === "/popisSvihKvarova" || location.pathname === "/popisAktivnihKvarova" ? "active" : ""}`} role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <MdHandyman />
-                  Popis kvarova
-                </button>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className=" icon-link dropdown-item" aria-current="page" to="/popisSvihKvarova">
-                      <FaList />
-                      Popis svih kvarova
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className=" icon-link dropdown-item" aria-current="page" to="/popisAktivnihKvarova">
-                      <FaList />
-                      Aktivni kvarovi
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li>
-            <Link className={`nav-link icon-link ${location.pathname === "/popisObjekata" ? "active" : ""}`} aria-current="page" to="/popisObjekata">
-              <FaRegBuilding />
-              Popis objekata
-            </Link>
-            </li>
-            <li>
-            <Link className={`nav-link icon-link ${location.pathname === "/popisSoba" ? "active" : ""}`} aria-current="page" to="/popisSoba">
-              <TbDoor />
-              Popis soba
-            </Link>
-            </li>
-            <li>
-            <Link className={`nav-link icon-link ${location.pathname === "/popisKreveta" ? "active" : ""}`} aria-current="page" to="/popisKreveta">
-              <FaBed />
-              Popis kreveta
-            </Link>
-            </li>
-            <li>
-            <Link className={`nav-link icon-link ${location.pathname === "/unosKvarova" ? "active" : ""}`} aria-current="page" to="/unosKvarova">
-              <MdHandyman />
-              Prijava kvara
-            </Link>
-            </li>
+              {(isAdmin() || isRecepcionar()) && (
+                <li className="nav-item dropdown-center">
+                  <button className={`nav-link dropdown-toggle icon-link ${location.pathname === "/popisSvihStanara" || location.pathname === "/popisVremenskogPeroida" ? "active" : ""}`} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <PiStudent />
+                    Popisi studenata
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link className="icon-link dropdown-item" aria-current="page" to="/popisSvihStanara">
+                        <FaList />
+                        Popis svih trenutačnih stanara
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="icon-link dropdown-item" aria-current="page" to="/popisVremenskogPeroida">
+                        <FaList />
+                        Popis stanara u vremenskom periodu
+                      </Link>
+                    </li>
+                  </ul>
                 </li>
+              )}
+              {isAdmin() && (
+                <Link className={`nav-link icon-link ${location.pathname === "/PopisKorisnika" ? "active" : ""}`} aria-current="page" to="/PopisKorisnika">
+                  <FaRegUser />
+                  Popis korisnika
+                </Link>
+              )}
+              {(isAdmin() || isRecepcionar()) && (
+                <li>
+                  <Link className={`nav-link icon-link ${location.pathname === "/PopisBoravaka" ? "active" : ""}`} aria-current="page" to="/PopisBoravaka">
+                    <FaBookReader />
+                    Popis Boravaka
+                  </Link>
+                </li>
+              )}
+              {(isAdmin() || isDomar()) && (
+                <li className="nav-item dropdown-center">
+                  <button className={`nav-link dropdown-toggle icon-link ${location.pathname === "/popisSvihKvarova" || location.pathname === "/popisAktivnihKvarova" ? "active" : ""}`} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <MdHandyman />
+                    Popis kvarova
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link className="icon-link dropdown-item" aria-current="page" to="/popisSvihKvarova">
+                        <FaList />
+                        Popis svih kvarova
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="icon-link dropdown-item" aria-current="page" to="/popisAktivnihKvarova">
+                        <FaList />
+                        Aktivni kvarovi
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              )}
+              {isAdmin() && (
+                <li>
+                  <Link className={`nav-link icon-link ${location.pathname === "/popisObjekata" ? "active" : ""}`} aria-current="page" to="/popisObjekata">
+                    <FaRegBuilding />
+                    Popis objekata
+                  </Link>
+                </li>
+              )}
+              {isAdmin() && (
+                <li>
+                  <Link className={`nav-link icon-link ${location.pathname === "/popisSoba" ? "active" : ""}`} aria-current="page" to="/popisSoba">
+                    <TbDoor />
+                    Popis soba
+                  </Link>
+                </li>
+              )}
+              {isAdmin() && (
+                <li>
+                  <Link className={`nav-link icon-link ${location.pathname === "/popisKreveta" ? "active" : ""}`} aria-current="page" to="/popisKreveta">
+                    <FaBed />
+                    Popis kreveta
+                  </Link>
+                </li>
+              )}
+              {isStanar() && (
+                <li>
+                  <Link className={`nav-link icon-link ${location.pathname === "/unosKvarova" ? "active" : ""}`} aria-current="page" to="/unosKvarova">
+                    <MdHandyman />
+                    Prijava kvara
+                  </Link>
+                </li>
+              )}
+              {!isAuthenticated() && (
+                <li>
+                  <Link className={`nav-link icon-link ${location.pathname === "/prijava" ? "active" : ""}`} aria-current="page" to="/prijava">
+                    <IoIosLogIn />
+                    Prijava
+                  </Link>
+                </li>
+              )}
+              {tokenExists && (
+                <li>
+                  <button className="nav-link text-danger" onClick={handleLogout}>
+                    <IoIosLogOut />
+                    Odjava
+                  </button>
+                </li>
+              )}
             </ul>
-            <Link className={`nav-link icon-link ${location.pathname === "/prijava" ? "active" : ""}`} aria-current="page" to="/prijava">
-              <IoIosLogIn />
-              Prijava
-            </Link>
           </div>
         </div>
       </div>
--- react-app/src/pages/PrijavaPage.jsx
+++ react-app/src/pages/PrijavaPage.jsx
@@ -1,40 +1,94 @@
-import { useState } from 'react';
+import { useState } from "react";
+import axios from "axios"; // Import axios for API requests
 import CollapsableNavbar from "../components/CollapsableNavbar";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const PrijavaPage = () => {
-  const [Email, setEmail] = useState('');
-  const [Lozinka, setLozinka] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Add your login logic here
-    console.log('Email:', Email);
-    console.log('Lozinka:', Lozinka);
+    try {
+      // Send a POST request to the login endpoint
+      const response = await axios.post("http://localhost:3000/login", {
+        email_korisnika: email,
+        lozinka: password,
+      });
+
+      if (response.data.success) {
+        // If login is successful, save the token in local storage
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        console.log("Login successful");
+
+        // Decode the token to get the user's role
+        const decodedToken = jwtDecode(token);
+        const userRole = decodedToken.uloga;
+
+        // Redirect the user based on their role
+        if (userRole === "stanar") {
+          navigate("/unosKvarova");
+        } else if (userRole === "admin" || userRole === "recepcionar") {
+          navigate("/popisSvihStanara");
+        } else if (userRole === "domar") {
+          navigate("/popisAktivnihKvarova");
+        }
+      } else {
+        // Display an error message if login fails
+        setErrorMessage(response.data.message);
+      }
+    } catch (error) {
+      // Handle any other errors (e.g., network errors)
+      console.error("Error during login:", error);
+      setErrorMessage("Internal server error");
+    }
   };
 
   return (
     <div>
       <CollapsableNavbar />
-      <div className="container">
-        <h1 className="mt-4">Prijavi se</h1>
-        <div className="row justify-content-center mt-5">
-          <div className="col-md-6">
-            <form onSubmit={handleSubmit}>
-              <div className="mb-3">
-                <label htmlFor="Email" className="form-label">Email:</label>
-                <input type="text" className="form-control" id="Email" value={Email} onChange={(e) => setEmail(e.target.value)} required />
+      <section className="vh-100 gradient-custom">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div className="card bg-dark text-white">
+                <div className="card-body p-5 text-center">
+                  <div className="mb-md-5 mt-md-4 pb-5">
+                    <h2 className="fw-bold mb-2 text-uppercase">Prijava</h2>
+                    <p className="text-white-50 mb-5">Molimo unesite svoje podatke za prijavu</p>
+
+                    <form onSubmit={handleSubmit}>
+                      <div className="form-outline form-white mb-4">
+                        <input type="email" id="typeEmailX" className="form-control form-control-lg" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        <label className="form-label" htmlFor="typeEmailX">
+                          Email
+                        </label>
+                      </div>
+
+                      <div className="form-outline form-white mb-4">
+                        <input type="password" id="typePasswordX" className="form-control form-control-lg" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        <label className="form-label" htmlFor="typePasswordX">
+                          Lozinka
+                        </label>
+                      </div>
+
+                      <button data-mdb-button-init data-mdb-ripple-init className="btn btn-outline-light btn-lg px-5" type="submit">
+                        Prijavi se
+                      </button>
+                    </form>
+
+                    {errorMessage && <div className="text-danger mt-3">{errorMessage}</div>}
+                  </div>
+                </div>
               </div>
-              <div className="mb-3">
-                <label htmlFor="Lozinka" className="form-label">Lozinka</label>
-                <input type="password" className="form-control" id="Lozinka" value={Lozinka} onChange={(e) => setLozinka(e.target.value)} required />
-              </div>
-              <div className="d-grid">
-                <button type="submit" className="btn btn-primary">Login</button>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
